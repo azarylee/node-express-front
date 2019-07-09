d73b79c8c772a6cd6b904e19d0c76a982db6ba17
azarylee <liyijun430@hotmail.com>
2019-07-09 14:36:15 +1000

    build

---
+++ dist/static/js/0.549a04c1a04a2a29dc12.js
@@ -0,0 +1,2 @@
+webpackJsonp([0],{Dnmp:function(t,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var s={name:"me",data:function(){return{list:[]}},mounted:function(){var t=this;this.$store.dispatch("getMe").then(function(e){console.log(e),t.list=e}).catch(function(t){console.log(t)})}},i={render:function(){var t=this.$createElement;return(this._self._c||t)("div",[this._v("\n    "+this._s(this.list)+"\n")])},staticRenderFns:[]},l=n("VU/8")(s,i,!1,null,null,null);e.default=l.exports}});
+//# sourceMappingURL=0.549a04c1a04a2a29dc12.js.map
